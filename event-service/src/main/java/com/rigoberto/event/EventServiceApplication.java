ff8c962819cdde2cb989e62ff00b86530a33a6eb
Rigoberto Pérez Rajadel <rigotra1984@gmail.com>
2023-08-18 12:13:03 -0400

    ajustando

--- event-service/src/main/java/com/rigoberto/event/EventServiceApplication.java
+++ event-service/src/main/java/com/rigoberto/event/EventServiceApplication.java
@@ -1,5 +1,7 @@
 package com.rigoberto.event;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
@@ -8,6 +10,7 @@ import org.springframework.web.reactive.config.EnableWebFlux;
 @EnableR2dbcRepositories
 @EnableWebFlux
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Event API", version = "1.0", description = "Documentation Event API v1.0"))
 public class EventServiceApplication {
 
 	public static void main(String[] args) {
